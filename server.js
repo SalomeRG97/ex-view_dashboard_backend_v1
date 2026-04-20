7ec5e903463a6ae7c31344818549a389a4654757
SalomeRG97 <salomeruizgallego@outlook.com>
2026-04-20 18:21:13 -0500

    fix: resolve server-side connection errors in dashboard backend

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = 3000;
 app.use(cors({
   origin: [
     'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    'http://localhost:5500',
+    'https://ex-view.com'
   ]
 })); app.use(express.json());
 
